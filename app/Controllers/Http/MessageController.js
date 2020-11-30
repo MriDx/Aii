d96f223c4471426a8bca7967339e1d287dab019b
mridx <mridulbaishya28@gmail.com>
2020-11-30 23:37:41 +0530

    changed cors

--- app/Controllers/Http/MessageController.js
+++ app/Controllers/Http/MessageController.js
@@ -4,19 +4,21 @@ const Message = use('App/Models/Message')
 
 class MessageController {
 
-	async create({request, response, session, view}) {
+	async create({request, response, auth, session, view}) {
 		const {name, email, message} = request.all()
 		try {
+			//const user = await auth.login(email, name)
+
 			await Message.create({
 				name: name,
 				email: email,
 				message: message
 			})
 
-			session.flash({ message: 'Your message has been posted !' });
+			session.flash({ message: 'Your message has been posted ! ' + user });
 			return response.redirect('back');
 		} catch (error) {
-			session.flash({ message: 'something went wrong !å' });
+			session.flash({ message: 'something went wrong !' + error });
 			return response.redirect('back');
 		}
 
